cd7850dc224bcb15c6004ac217dc83ec5c941877
famturiop <vorobyev.seva.rules@gmail.com>
2021-03-17 23:20:44 +0300

    money investing app fix #1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -15,17 +15,18 @@ namespace ConsoleApp1
             double percent = (rand.Next(-1000,1000));
             percent = percent / 1000;
             int year = 0;
+            double commision = 0.1;
 
             while (cash > 0 || money_in_bank > 0)
                 {
 
-                    Console.WriteLine($"It is {year} year. You currently have {cash} in cash and {money_in_bank} in a bank. Do you want to invest it with a yearly rate of {percent} gain ? (Y/N)");
+                    Console.WriteLine($"It is {year} year. You currently have {cash} in cash and {money_in_bank} in a bank. Bank commision is {commision}. Do you want to invest it with a yearly rate of {percent} gain ? (Y/N)");
                     invest = Console.ReadLine();
 
                     if (invest == "Y" || invest == "y")
                     {
                         
-                        money_in_bank = cash * (1 + percent);
+                        money_in_bank = (cash+money_in_bank) * (1 + percent) - commision;
                         cash = 0;
                     }
                     else
@@ -38,6 +39,7 @@ namespace ConsoleApp1
                 percent = percent / 1000;
                 year++;
                 }
+            Console.WriteLine("Congratulations! You are bankrupt.");
         }
     }
 }
